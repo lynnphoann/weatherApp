246cde5915b9eea7352a0989e19e0edf8ec4438c
lynnphoann <45825771+lynnphoann@users.noreply.github.com>
2022-09-27 00:46:02 +0630

    sec update

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/model/weatherModel.dart';
 import 'package:weather_app/weatherApi.dart';
 
 void main() {
@@ -31,47 +32,59 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   WeatherApi client = WeatherApi();
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    client.getCurrentWeather("japan");
+  Weather? apiData;
+
+  Future<void> getApiData() async {
+    apiData = await client.getCurrentWeather("japan");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Center(
-            child: Container(
-              height: MediaQuery.of(context).size.height * 0.2,
-              child: Text(
-                "London",
-                style: TextStyle(fontSize: 25),
-              ),
-            ),
-          ),
-          const Divider(),
-          Container(
-            height: 100,
-            child: GridView(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, mainAxisExtent: 50),
-              children: [
-                Text("Temp       14*"),
-                Text("Feels like 15*"),
-                Text("wind speed 2.06*"),
-                Text("humidity   57*")
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: FutureBuilder(
+          future: getApiData(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Center(
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.2,
+                      child: Text(
+                        "${apiData!.cityName}",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    ),
+                  ),
+                  const Divider(),
+                  Container(
+                    height: 100,
+                    child: GridView(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2, mainAxisExtent: 50),
+                      children: [
+                        Text("Temp       ${apiData!.temp}"),
+                        Text("Feels like ${apiData!.feelLike}"),
+                        Text("wind speed ${apiData!.windspeed}"),
+                        Text("humidity   ${apiData!.humidity}"),
+                      ],
+                    ),
+                  )
+                ],
+              );
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return Container();
+            }
+          },
+        ));
   }
 }
